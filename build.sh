3a94047ced136ea04cffe38e5561bdcb244a0469
sasha1517 <0508236483k@gmail.com>
2026-05-25 22:49:08 +0300

    Fix superuser creation

--- build.sh
+++ build.sh
@@ -5,4 +5,9 @@ python manage.py migrate --noinput
 python manage.py collectstatic --noinpu
 python manage.py createsuperuser --noinput || true
 gunicorn --bind 0.0.0.0:$PORT mysite.wsgi:application
+python manage.py shell << EOF
+from django.contrib.auth import get_user_model
+User = get_user_model()
+if not User.objects.filter(username='admin').exists():
+    User.objects.create_superuser('admin', 'admin@test.com', 'Qwerty12345!')
 EOF

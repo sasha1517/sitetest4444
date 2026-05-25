f0b279a3e6037fac185d19df7434466f5401f7f9
sasha1517 <0508236483k@gmail.com>
2026-05-25 22:34:17 +0300

    Auto-create superuser on deploy

--- build.sh
+++ build.sh
@@ -3,6 +3,6 @@ cat > build.sh << 'EOF'
 set -e
 python manage.py migrate --noinput
 python manage.py collectstatic --noinpu
-python manage.py createsuperuser --noinput || truet
+python manage.py createsuperuser --noinput || true
 gunicorn --bind 0.0.0.0:$PORT mysite.wsgi:application
 EOF
